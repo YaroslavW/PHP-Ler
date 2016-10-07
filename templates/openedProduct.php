2bc0e68c60257ba7c0e5c44a238378e2d225ef2f
YaroslavW <doctor355@yahoo.com>
2016-10-07 22:50:31 +0300

    first commit

---
+++ templates/openedProduct.php
@@ -0,0 +1,15 @@
+            <div id="openedProduct-img">
+                <img src="<?php echo $good['img'];?>">
+            </div>
+            <div id="openedProduct-content">
+                <h1 id="openedProduct-name">
+                    <?php echo $good['name']; ?>
+                </h1>
+                <div id="openedProduct-desc">
+                   <?php echo $good['desk']; ?>
+                </div>
+                <div id="openedProduct-price">
+                    Цена: <?php echo $good['price']; ?>
+                </div>
+            </div>
+
